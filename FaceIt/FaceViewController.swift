e86d2f61c6ae6a6c6838703ca1f8f161d7285b03
Renat Khalikov <renavoo@gmail.com>
2016-12-30 15:13:24 -0500

    Face Drawing App
    Based off Stanford’s Developing iOS Apps with Swift

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Private on 12/30/16.
+//  Copyright © 2016 Private. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    
+
+
+}
+
